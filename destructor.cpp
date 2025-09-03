d40041c0ef3b0b5ca6fc0b91289985e3432c4999
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-09-03 05:24:42 -0400

    Create destructor.cpp

---
+++ destructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int count = 0;
+
+    class destruct{
+    public:
+    destruct()
+    {
+        count ++;
+        cout <<"No. of objects created: "<<count<<endl;
+    }
+    ~destruct()
+    {
+        count--;
+        cout<<"No. of objects destroyed: "<<count<<endl;
+    }
+    };
